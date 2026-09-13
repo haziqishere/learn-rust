a81bf2dd88c228edf6fae0e7f832d86e249f9ec3
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2026-09-13 19:42:40 +0800

    add/hello world

---
+++ chp0-rustHello/helloProject/target/debug/deps/helloProject-0d4e69d3e9eeeacc.d
@@ -0,0 +1,5 @@
+/Users/haziq/Developer/Hobby/learn-rust/rustHello/helloProject/target/debug/deps/helloProject-0d4e69d3e9eeeacc.d: src/main.rs
+
+/Users/haziq/Developer/Hobby/learn-rust/rustHello/helloProject/target/debug/deps/helloProject-0d4e69d3e9eeeacc: src/main.rs
+
+src/main.rs:
---
+++ chp0-rustHello/helloProject/target/debug/helloProject.d
@@ -0,0 +1,1 @@
+/Users/haziq/Developer/Hobby/learn-rust/rustHello/helloProject/target/debug/helloProject: /Users/haziq/Developer/Hobby/learn-rust/rustHello/helloProject/src/main.rs
