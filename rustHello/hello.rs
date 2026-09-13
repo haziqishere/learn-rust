bd4b42cb2b1d13e44d750dfd8e620ef41fe4ca02
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2026-09-13 19:38:44 +0800

    add/hello-world

---
+++ rustHello/hello.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// 2 Ways to run the code
+
+// Method 1: Use rustc
+// Step 1:
+// execute hello.rs via `rustc hello.rs`
+// it'll create a binary file named `hello`. This is executable file
+
+// Step 2:
+// run the executable `./hello`
+
+// Method 2: Use cargo
+
+// Step 1:
+// create a new project via `cargo new helloProject``
+
+// Step 2:
+// cd into the project directory and run the executable `cargo run`
